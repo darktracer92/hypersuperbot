16097bd205b9358a2979eea8ae3481ef9e32835e
mbattisti4 <mbattisti4@hotmail.com>
2017-12-04 16:54:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -17,7 +17,7 @@ $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
-if(strpos($text, "/start") === 0 || strtolower($text) =="ciao")
+/*if(strpos($text, "/start") === 0 || strtolower($text) =="ciao")
 {
 	$response = "Ciao $firstname, benvenuto!";
 }
@@ -32,7 +32,20 @@ elseif($text=="domanda 2")
 else
 {
 	$response = "Comando non valido!";
-}
-$parameters = array('chat_id' => $chatId, "text" => $response);
+}*/
+
+//----------------------------
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
 $parameters["method"] = "sendMessage";
+// imposto la inline keyboard
+$keyboard = ['inline_keyboard' => [[['text' =>  'myTex1t', 'callback_data' => 'myCallbackText']]]];
+$parameters["reply_markup"] = json_encode($keyboard, true);
+
+
+/*$parameters = array('chat_id' => $chatId, "text" => $response);
+$parameters["method"] = "sendMessage";*/
 echo json_encode($parameters);
